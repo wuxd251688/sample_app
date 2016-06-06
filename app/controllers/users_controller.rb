4719e45d6915d7497bc2c72ddf540839985e5904
wuxd <36547863@qq.com>
2016-06-06 22:28:45 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -57,13 +59,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
   end
-  def signed_in_user
 
-    unless signed_in?
-      store_location
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-    end
 
   def correct_user
     @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   #before_save { email.downcase! }
+  has_many :microposts, dependent: :destroy
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
@@ -12,6 +13,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def feed
+# This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
 
   def User.new_remember_token
